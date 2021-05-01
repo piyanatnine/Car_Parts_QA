6e333f6f1e97219d213130adda86ef24173b5d43
piyanatnine <56292153+piyanatnine@users.noreply.github.com>
2021-05-02 06:02:41 +0700

    Update Homepage.vue

--- frontend/src/views/Homepage.vue
+++ frontend/src/views/Homepage.vue
@@ -5,7 +5,7 @@
         <a href="/">
           <div class="navbar-item pl-4">
             <img class="image is-32x32" src="../assets/img/icon.png" />
-            <span class="px-4 textIcon "> CarPart QA</span>
+            <span class="px-4 textIcon"> CarPart QA</span>
           </div>
         </a>
       </div>
@@ -110,6 +110,7 @@
                         <input
                           class="input"
                           type="text"
+                          v-model="searching[0]"
                           placeholder="Find Project"
                         />
                       </p>
@@ -123,56 +124,125 @@
                     </div>
                   </div>
                   <div class="level-item ml-2">
-                    <span class="icon">
-                      <i
-                        class="fa fa-sort-amount-desc fa-2x"
-                        aria-hidden="true"
-                      ></i>
-                    </span>
+                    <div class="dropdown is-hoverable">
+                      <div class="dropdown-trigger">
+                        <button
+                          class="button"
+                          aria-haspopup="true"
+                          aria-controls="dropdown-menu4"
+                        >
+                          <span class="icon">
+                            <i
+                              class="fa fa-sort-amount-desc fa-2x"
+                              aria-hidden="true"
+                            ></i>
+                          </span>
+                        </button>
+                      </div>
+                      <div
+                        class="dropdown-menu"
+                        id="dropdown-menu4"
+                        role="menu"
+                      >
+                        <div class="dropdown-content">
+                          <div class="dropdown-item">
+                              <label class="radio">
+                                <input type="radio" value="name" 
+                                v-model= "sort_function.sort_project"/>
+                                Name
+                              </label>
+                              <br>
+                              <label class="radio">
+                                <input type="radio" value="id"
+                                v-model= "sort_function.sort_project"/>
+                                ID
+                              </label>
+                              <br>
+                              <label class="radio">
+                                <input type="radio" value="status"
+                                v-model= "sort_function.sort_project"/>
+                                Status
+                              </label>
+                              <br>
+                              <label class="radio">
+                                <input type="radio" value="complete"
+                                v-model= "sort_function.sort_project"/>
+                                Complete
+                              </label>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </nav>
               <table class="homepage table is-fullwidth">
                 <thead>
                   <tr>
-                    <th style="width: 60%">Name</th>
+                    <th style="width: 40%">Name</th>
+                    <th style="width: 20%">ID</th>
                     <th>Status</th>
                     <th>Complete %</th>
                     <th style="width: 5"></th>
                   </tr>
                 </thead>
                 <tbody>
-                  <template v-if= "projects != null">
-                  <template v-for= "project in projects.slice(page * 5 - 5, page * 5)">
-                    <tr
-                      :class= "more[0] ? '' : 'delBorder'"
-                      :key= "`${project.project_id}-${project.project_name}`"
+                  <template v-if="projects != null">
+                    <template
+                      v-for="project in projectResult.slice(
+                        page * 5 - 5,
+                        page * 5
+                      )"
                     >
-                      <td class="is-size-4 has-text-weight-bold" @click= "loaderPage(`/${project.project_id}`)"> 
-                          {{ project.project_name}}
-                      </td>
-                      <td class="has-text-info is-uppercase pt-5 has-text-weight-bold">
-                        {{  project.completeness != "100" ? 'inprogress' : 'complete' }}
-                      </td>
-                      <td>
-                        <div :class= "'p' + integer(project.completeness)" class="c100 small">
-                          <span>{{ integer(project.completeness) + "%" }}</span>
-                          <div class="slice">
-                            <div class="bar"></div>
-                            <div class="fill"></div>
+                      <tr
+                        :class="more[0] ? '' : 'delBorder'"
+                        :key="`${project.project_id}-${project.project_name}`"
+                      >
+                        <td
+                          class="is-size-4 has-text-weight-bold"
+                          @click="loaderPage(`/${project.project_id}`)"
+                        >
+                          {{ project.project_name }}
+                        </td>
+                        <td
+                          class="is-size-5 has-text-weight-semibold pt-4"
+                          @click="loaderPage(`/${project.project_id}`)"
+                        >
+                          {{ zeroPad(project.project_id) }}
+                        </td>
+                        <td
+                          class="has-text-info is-uppercase pt-5 has-text-weight-bold"
+                        >
+                          {{
+                            project.completeness != "100"
+                              ? "inprogress"
+                              : "complete"
+                          }}
+                        </td>
+                        <td>
+                          <div
+                            :class="'p' + integer(project.completeness)"
+                            class="c100 small"
+                          >
+                            <span>{{
+                              integer(project.completeness) + "%"
+                            }}</span>
+                            <div class="slice">
+                              <div class="bar"></div>
+                              <div class="fill"></div>
+                            </div>
                           </div>
-                        </div>
-                      </td>
-                      <td class="has-text-centered pt-3"
-                          @click= "loaderPage(`/${project.project_id}`)">
-                        <span class="icon pt-5">
-                          <i
-                            class="fas fa-3x <i fas fa-caret-right">
-                          </i>
-                        </span>
-                      </td>
-                    </tr>
-                  </template>
+                        </td>
+                        <td
+                          class="has-text-centered pt-3"
+                          @click="loaderPage(`/${project.project_id}`)"
+                        >
+                          <span class="icon pt-5">
+                            <i class="fas fa-3x <i fas fa-caret-right"> </i>
+                          </span>
+                        </td>
+                      </tr>
+                    </template>
                   </template>
                 </tbody>
               </table>
@@ -216,6 +286,7 @@
                         <input
                           class="input"
                           type="text"
+                          v-model="searching[1]"
                           placeholder="Find Part"
                         />
                       </p>
@@ -229,12 +300,49 @@
                     </div>
                   </div>
                   <div class="level-item ml-2">
-                    <span class="icon">
-                      <i
-                        class="fa fa-sort-amount-desc fa-2x"
-                        aria-hidden="true"
-                      ></i>
-                    </span>
+                                        <div class="dropdown is-hoverable">
+                      <div class="dropdown-trigger">
+                        <button
+                          class="button"
+                          aria-haspopup="true"
+                          aria-controls="dropdown-menu4"
+                        >
+                          <span class="icon">
+                            <i
+                              class="fa fa-sort-amount-desc fa-2x"
+                              aria-hidden="true"
+                            ></i>
+                          </span>
+                        </button>
+                      </div>
+                      <div
+                        class="dropdown-menu"
+                        id="dropdown-menu4"
+                        role="menu"
+                      >
+                        <div class="dropdown-content">
+                          <div class="dropdown-item">
+                              <label class="radio">
+                                <input type="radio" value="number"
+                                v-model= "sort_function.sort_part"/>
+                                Number
+                              </label>
+                              <br>
+                              <label class="radio">
+                                <input type="radio" value="name" 
+                                v-model= "sort_function.sort_part"/>
+                                Name
+                              </label>
+                              <br>
+                              <label class="radio">
+                                <input type="radio" value="id"
+                                v-model= "sort_function.sort_part"/>
+                                Project ID
+                              </label>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </nav>
@@ -248,22 +356,34 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <template v-if= "parts != null">
-                  <template
-                    v-for="part in parts.slice(page * 10 - 10, page * 10)"
-                  >
-                    <tr :key="`${part.part_number}`" class="has-text-weight-semibold">
-                      <td>{{ zeroPad(part.part_number) }}</td>
-                      <td>{{ part.part_name }}</td>
-                      <td>{{ zeroPad(part.project_id) }}</td>
-                      <td>
-                        <button class="button is-link is-outlined" 
-                        @click= "loaderPage(`/${part.project_id}/${part.part_number}`)">
-                          readmore
-                        </button>
-                      </td>
-                    </tr>
-                  </template>
+                  <template v-if="parts != null">
+                    <template
+                      v-for="part in partResult.slice(
+                        page * 10 - 10,
+                        page * 10
+                      )"
+                    >
+                      <tr
+                        :key="`${part.part_number}`"
+                        class="has-text-weight-semibold"
+                      >
+                        <td>{{ zeroPad(part.part_number) }}</td>
+                        <td>{{ part.part_name }}</td>
+                        <td>{{ zeroPad(part.project_id) }}</td>
+                        <td>
+                          <button
+                            class="button is-link is-outlined"
+                            @click="
+                              loaderPage(
+                                `/${part.project_id}/${part.part_number}`
+                              )
+                            "
+                          >
+                            readmore
+                          </button>
+                        </td>
+                      </tr>
+                    </template>
                   </template>
                 </tbody>
               </table>
@@ -300,13 +420,18 @@
 </template>
 
 <script>
-console.clear()
+console.clear();
 import axios from "axios";
 import router from "../router/index.js";
 /* eslint-disable */
 export default {
   data() {
     return {
+      sort_function: {
+        sort_project: "id", //name,id,status,%
+        sort_part: "number", //number,name,projectId
+      },
+      searching: ["", ""],
       loginStatus: false, //loginStatus
       pageloader: "is-active",
       sidebar: true,
@@ -314,7 +439,7 @@ export default {
       more: [false, false],
       page: 1,
       projects: null,
-      parts: null
+      parts: null,
     };
   },
   methods: {
@@ -329,7 +454,7 @@ export default {
       });
 
       this.pageloader = await loader;
-      
+
       try {
         await setTimeout(() => {
           router.push({ path: pathname });
@@ -340,44 +465,145 @@ export default {
       }
     },
     pageCount(listVal) {
-      let val = 0
-      if (listVal == 'parts') {
-        val = Math.ceil(this.parts.length / 10);
+      let val = 0;
+      if (listVal == "parts") {
+        val = Math.ceil(this.partResult.length / 10);
+      } else if (listVal == "project") {
+        val = Math.ceil(this.projectResult.length / 5);
       }
-      else if(listVal == 'project') {
-        val = Math.ceil(this.projects.length / 5);
-        console.log('page: '+ val)
-      }
-      
+
       return val;
     },
     zeroPad(num) {
       return num.toString().padStart(8, "0");
     },
-    integer(val){
-      return parseInt(val)
+    integer(val) {
+      return parseInt(val);
+    },
+    //sort_function
+    sort_id(select) {
+      function compare(a, b){
+        if (a.project_id < b.project_id){return -1}
+        else if (a.project_id > b.project_id){return 1}
+        else {return 0}
+      }
+
+      if(select){
+        this.projects.sort(compare)
+      } else {
+        this.parts.sort(compare)
+      }
+    },
+    sort_project_name() {
+      function compare(a, b){
+        if (a.project_name < b.project_name){return -1}
+        else if (a.project_name > b.project_name){return 1}
+        else {return 0}
+      }
+      this.projects.sort(compare)
+    },
+    sort_part_name(){
+      function compare(a, b){
+        if (a.part_name < b.part_name){return -1}
+        else if (a.part_name > b.part_name){return 1}
+        else {return 0}
+      }
+      this.parts.sort(compare)
+    },
+    sort_complete() {
+      function compare(a, b){
+        if (a.completeness < b.completeness){return -1}
+        else if (a.completeness > b.completeness){return 1}
+        else {return 0}
+      }
+      this.projects.sort(compare)
+    },
+    sort_status() {
+      function compare(a, b){
+        if (a.status < b.status){return -1}
+        else if (a.status > b.status){return 1}
+        else {return 0}
+      }
+      this.projects.sort(compare)
+    },
+    sort_number(){
+      function compare(a, b){
+        if (a.part_number < b.part_number){return -1}
+        else if (a.part_number > b.part_number){return 1}
+        else {return 0}
+      }
+      this.parts.sort(compare)
     }
   },
   watch: {
     listItem: function (val) {
-        this.page = 1 
-    }
+      this.page = 1;
+    },
+  },
+  computed: {
+    projectResult() {
+      if (this.sort_function.sort_project == 'name'){
+        this.sort_project_name(true);
+      }
+      if (this.sort_function.sort_project == 'id'){
+        this.sort_id(true);
+      }
+      if (this.sort_function.sort_project == 'status'){
+        this.sort_status(true);
+      }
+      if (this.sort_function.sort_project == 'complete'){
+        this.sort_complete(true);
+      }
+
+      var result = this.projects;
+      var txt = this.searching[0].toUpperCase();
+      result = result.filter((val) => {
+        if (
+          val.project_name.toUpperCase().indexOf(txt) > -1 ||
+          val.project_id.toString().indexOf(txt) > -1
+        ) {
+          return val;
+        }
+      });
+      return result;
+    },
+    partResult() {
+      if (this.sort_function.sort_part == 'name'){
+        this.sort_part_name();
+      }
+      if (this.sort_function.sort_part == 'id'){
+        this.sort_id(false);
+      }
+      if (this.sort_function.sort_part == 'number'){
+        this.sort_number();
+      }
+
+      var result = this.parts;
+      var txt = this.searching[1].toUpperCase();
+      result = result.filter((val) => {
+        if (
+          val.part_name.toUpperCase().indexOf(txt) > -1 ||
+          val.part_number.toString().indexOf(txt) > -1
+        ) {
+          return val;
+        }
+      });
+      return result;
+    },
   },
   created: async function () {
-   await axios.get("http://localhost:3000/")
-        .then((response) => {
-          this.projects = response.data.projects;
-          this.parts = response.data.parts;
-          this.projects.map( value => {
-              var a = {
-                more: false
-              } 
-              value.status_page = a;
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-    });
+    await axios
+      .get("http://localhost:3000/")
+      .then((response) => {
+        this.projects = response.data.projects;
+        this.parts = response.data.parts;
+        this.projects.map((value) => {
+          value.search_page = true;
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
     let afterloader = new Promise(function (myResolve) {
       setTimeout(() => {
@@ -385,8 +611,7 @@ export default {
       }, 1000);
     });
     this.pageloader = await afterloader;
-
-  }
+  },
 };
 </script>
 
@@ -410,8 +635,8 @@ section .main-content {
   animation: slideDisplay 0.5s ease;
 }
 
-.homepage tr:hover td{
-  background-color:hsl(0, 0%, 86%);
+.homepage tr:hover td {
+  background-color: hsl(0, 0%, 86%);
 }
 
 @keyframes slideDisplay {

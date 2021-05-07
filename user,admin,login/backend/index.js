23d20d52ebf3665b366716cef863eb947a28a131
it62070112kmitl <81900185+it62070112kmitl@users.noreply.github.com>
2021-05-07 13:10:04 +0700

    Add files via upload

---
+++ user,admin,login/backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+var cors = require('cors')
+const path = require("path")
+
+const app = express();
+app.use(cors())
+// Statics
+app.use(express.static('static'))
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// routers
+const indexRouter = require('./routes/index')
+
+app.use(indexRouter.router)
+
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:3000`)
+})
